34b48ea5183f0ba00e109ca0009abbb8063289f2
jrodriguezra <jrodriguezra@indracompany.com>
2022-04-06 22:09:00 -0500

    Agregando proyecto angular al proyecto y creación de modulo user de prueba

---
+++ FrontEnd/ClientApp/src/app/user/user.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-usuario',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.css']
+})
+export class UserComponent {
+  nombreusuario = 'jhonathan Rodriguez Ramirez';
+}
